9f5d6d434bd2339b6fc7a8d699c8c4f1efd54062
jan <j.muczyns@stud.elka.pw.edu.pl>
2016-05-27 23:45:15 +0200

    small step for man giant leap for mankind :heart:

--- src/LindaLang/Pipe.cpp
+++ src/LindaLang/Pipe.cpp
@@ -1,14 +1,64 @@
-
 #include "Pipe.h"
 
-void Pipe::write(const void *buf, unsigned int len)
+Pipe::Pipe()
 {
-    // TODO
+    if(pipe(pipeDescriptors) == -1)
+        throw "";
 }
 
-void Pipe::read(void *buf, unsigned int len)
+Pipe::Pipe(int readDescr, int writeDescr)
 {
-    // TODO
+    pipeDescriptors[PipeEnd::ReadEnd] = readDescr;
+    pipeDescriptors[PipeEnd::WriteEnd] = writeDescr;
+}
+
+Pipe::~Pipe()
+{
+    Close(PipeEnd::ReadEnd);
+    Close(PipeEnd::WriteEnd);
+}
+
+int Pipe::Close(PipeEnd pe)
+{
+    close(pipeDescriptors[pe]);
+}
+
+void Pipe::writePipe(const void *buf, unsigned int len)
+{
+    if(len > PIPE_BUF)
+        throw ("Too big message: " + len);
+
+    if(write(pipeDescriptors[PipeEnd::WriteEnd], buf, len) == -1)
+        throw "Error at writing pipe";
+}
+
+int Pipe::readPipe(void *buf, unsigned int len)
+{
+    int result;
+    unsigned remaining = len;
+
+    do
+    {
+        result = read(pipeDescriptors[PipeEnd::ReadEnd], buf, len);
+
+        if(result == -1)
+            throw "Error at reading pipe";
+        else if(result == 0)
+        {
+            if(remaining == len)
+            {
+                Close(PipeEnd::ReadEnd);
+                return false;
+            }
+            else
+                throw "Incomplete reading pipe";
+        }
+
+        remaining -= result;
+
+    } while( remaining > 0);
+
+    return true;
 }
 
 
