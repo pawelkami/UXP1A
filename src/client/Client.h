cb84732e2fa445f9bd7f5bca2f00562d9820dd63
pawelkami <pawelkami2@gmail.com>
2016-05-30 00:55:31 +0200

    Dodano puste klasy Server i Client

---
+++ src/client/Client.h
@@ -0,0 +1,12 @@
+
+#ifndef UXP1A_CLIENT_H
+#define UXP1A_CLIENT_H
+
+
+class Client
+{
+
+};
+
+
+#endif //UXP1A_CLIENT_H
---
+++ src/server/Server.h
@@ -0,0 +1,19 @@
+
+#ifndef UXP1A_SERVER_H
+#define UXP1A_SERVER_H
+
+
+#include "TupleSpace.h"
+
+class Server
+{
+private:
+    TupleSpace tupleSpace;
+
+public:
+    
+
+};
+
+
+#endif //UXP1A_SERVER_H
