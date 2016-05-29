b74a2154d4072203fd64be9325d234b37d18d687
jan <j.muczyns@stud.elka.pw.edu.pl>
2016-05-29 03:33:05 +0200

    added methods to TupleSpace class

--- src/server/TupleSpace.cpp
+++ src/server/TupleSpace.cpp
@@ -1,2 +1,30 @@
-
 #include "TupleSpace.h"
+
+TupleSpace::TupleSpace()
+{
+
+}
+
+void TupleSpace::insertTuple(Tuple& tuple)
+{
+    tuples.push_back(tuple);
+}
+
+Tuple TupleSpace::getTuple(TuplePattern& pattern)
+{
+    return *findTuple(pattern);
+}
+
+void TupleSpace::removeTuple(TuplePattern& tuple)
+{
+    tuples.erase(findTuple(tuple));
+}
+
+std::vector<Tuple>::const_iterator TupleSpace::findTuple(TuplePattern& pattern)
+{
+    for(auto &it : tuples)
+    {
+
+    }
+    return nullptr;
+}
--- src/server/TupleSpace.h
+++ src/server/TupleSpace.h
@@ -3,14 +3,27 @@
 #define UXP1A_TUPLESPACE_H
 
 #include "Tuple.h"
+#include "TuplePattern.h"
 
 /**
  * Klasa reprezentująca przestrzeń krotek
  */
 class TupleSpace
 {
+public:
+
+    TupleSpace();
+
+    void insertTuple(Tuple& tuple);
+
+    Tuple getTuple(TuplePattern& pattern);
+
+    void removeTuple(TuplePattern& pattern);
+
 private:
     std::vector<Tuple> tuples;
+
+    std::vector<Tuple>::const_iterator findTuple(TuplePattern& tuple);
 
 };
 
